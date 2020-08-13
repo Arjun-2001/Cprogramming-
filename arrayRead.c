93ac4d6e13334d4a9f3ff7d92403fe7702c0c968
Arjun Dhillon <arjundhillon@Arjuns-MacBook-Pro.local>
2020-08-12 22:10:21 -0400

    removed unused code

--- arrayRead.c
+++ arrayRead.c
@@ -39,64 +39,6 @@ int main(){
 			printArray(numARR,SEL);
 
 }
-/*
-		  //open file(use w to make file switch to r for use
-		  FILE* fp = NULL;
-		  fp = fopen("numbers.txt","r");
-
-		  if (fp == NULL){
-					 printf("\n no file found \n");
-					 return(0);
-		  }else{
-					 //search until end of file
-					 while(numCurrent != EOF){
-								fscanf(fp, "%d", &numARR[x]);	
-
-								//checking each number in the array sequentially			
-								if(numARR[x]== user){
-										  printf("the program found a match");
-										  return(0);
-								}
-					 }
-		  }
-}
-
-//if search doesnt work end program
-printf("the program could not find a  match");
-fclose(fp);
-//	end = clock();
-return(0);
-}
-
-
-
-
-
-i tried my best but could not incooperate this method this was my method
-
-int middle;
-int numCurrent= 0;
-middle=SEL/2;
-
-if(user>=middle){
-for(numCurrent=0; numCurrent != EOF;numCurrent++){
-if(numARR[x]== user){
-printf("number has been found");
-}else{
-numCurrent++;
-}
-
-if(user<=middle){
-for(numCurrent=0; numCurrent != EOF;numCurrent++){
-if(numARR[x]== user){
-printf("number has been found");
-}else{
-numCurrent--;
-}
-}
-
-
- */
 
 void printArray(int numARR[],int size){
 	int i;
