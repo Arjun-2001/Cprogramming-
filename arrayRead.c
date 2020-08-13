8f56e7fed10f9734e7698b7ebe2818269ae49c61
Arjun Dhillon <arjundhillon@Arjuns-MacBook-Pro.local>
2020-08-13 04:48:23 -0400

    added linear and binary search function (bin used youtube tutorials to explain formula since it was not clear)

--- arrayRead.c
+++ arrayRead.c
@@ -7,18 +7,10 @@
 void printArray(int numARR[],int size);
 void initArray(int numARR[],int size);
 void inputArray(FILE* fp, int numARR[]);
+void checkNum(int user, int numARR[],int size);
+int checkNumBin(int user, int numARR[],int size);
 
 int main(){
-		  //errors:scanf wont stop scanning, readin in int list occurs incorrectly, bin search method could not use or test but i wrote my attempt in comments 
-
-		  //i cannot get this program to work
-
-
-		  //using time.h attempt to count runtime 
-		  //		clock_t start, end;
-		  //   	double cpu_time_used;
-		  //		start = clock();
-
 		  //declare array and var
 		  int numARR[SEL];
 		  int numCurrent;
@@ -26,7 +18,7 @@ int main(){
 		  int user;
 
 			initArray(numARR,SEL);
-         printArray(numARR,SEL);
+        // printArray(numARR,SEL);
 
 		  //ask user for search
 		  printf("\n select a number to search:");
@@ -36,8 +28,9 @@ int main(){
 			FILE* fp= NULL;
 			fp =fopen("numbers.txt","r");
 			inputArray(fp, numARR);
-			printArray(numARR,SEL);
-
+			//	printArray(numARR,SEL);
+			checkNum(user,numARR,SEL);
+			checkNumBin(user,numARR,SEL);
 }
 
 void printArray(int numARR[],int size){
@@ -59,6 +52,41 @@ int i = 0;
 
 while(fscanf(fp,"%d",&numARR[i])!= EOF){
 i++;
+	}
 }
+
+void checkNum(int user, int numARR[],int size){
+int i=0;
+int state = 0;
+for(i=0;i < size;i++){
+	if(user == numARR[i]){
+		printf("\nthe number you selected (%d) has been found",user);
+		state=1;
+		}
+	}
+	if(state==0){
+	printf("\nnumber not found in file\n");
+	}
+}
+
+int checkNumBin(int user, int numARR[],int size){
+int high = size-1;
+int low = 0;
+
+	while(low<=high){
+	int index = (high + low)/2;
+		if(user == numARR[index]){
+		printf("\nnumber (%d) has been found using binary search\n\n",user);
+		return(1);
+		}else{
+				if(user<numARR[index]){
+				high = index-1;
+		}else{
+				low = index+1;
+		}
+		}
+	}
+	printf("binary search for (%d) could not be found\n\n",user);
+	return(0);
 }
 
